61fb1bdbbba45177e1ae7a2e5346f0494c1b1b1e
fffw1989 <fffw1989@gmail.com>
2017-07-19 19:47:45 +0800

    better photos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,9 +38,9 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     if !current_cart.products.include?(@product)
        current_cart.add_product_to_cart(@product)
-       flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
+       flash[:notice] = "你已成功将 #{@product.title} 加入购物车!"
     else
-      flash[:warning] = "你的购物车已有此物品"
+      flash[:warning] = "你的购物车已有此物品!"
     end
     redirect_to :back
   end
